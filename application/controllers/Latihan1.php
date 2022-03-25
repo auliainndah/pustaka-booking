7949ac5265e350137528e6eb6acff8e96cb7f02e
auliainndah <auliaindah1219@gmail.com>
2022-03-25 14:19:25 +0900

    menambahkan controllers,models,views

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -3,15 +3,21 @@ class Latihan1 extends CI_Controller
 {
     public function index()
     {
-        echo "Selamat Datang Aulia selamat belajar Web Programming";
-        //$this->load->view('view-latihan1'); 
+        echo "Selamat datang.. Selamat belajar web programming";
+        //$this->load ->view (view-latihan1');
     }
 
+    public function penjumlahan($n1,$n2)
+    {
+        $this ->load ->model ('Model_latihan1');
+        //$hasil = $this-> Model_latihan1-> jumlah ($n1,$n2);
+        //echo " Hasil Penjumlahan dari". $n1 ."+". $n2 . "=" .$hasil;
+       
+        $data['nilai1'] =$n1;
+        $data['nilai2'] =$n2;
+        $data['hasil']  =$this ->Model_latihan1 -> jumlah ($n1, $n2);
 
-public function penjumlahan($n1, $n2)
-{
-    $this->load->model('Model_latihan1');
-    $hasil = $this->Model_latihan1->jumlah($n1, $n2);
-    echo "Hasil Penjumlahan dari" . $n1 ."  + ". $n2 ." = ".$hasil;
-}
+        $this -> load ->view ('view-latihan',$data);
+
+    }
 }
