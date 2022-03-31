684ce6932ca6a7cd7ee11e6e83de4429adcc43a7
auliainndah <auliaindah1219@gmail.com>
2022-04-01 00:17:21 +0900

    menambahkan Web.php,v_about.php,v_header.php,v_footer.php,v_index.php

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<selection>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Aulia</li>
+        <li>Nama Belakang : Indah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Sawah Barat 1 Duren Sawit</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Demak</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Renang</li>
+    </ul>
+</selection>
